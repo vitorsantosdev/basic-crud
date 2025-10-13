8d2e6acf48bb5f613f9acc8e42eb70ec8388aa26
Vitor <vitorsantos.br@gmail.com>
2025-10-13 21:43:23 +0100

    form datas in const *test

--- basic-crud/src/App.tsx
+++ basic-crud/src/App.tsx
@@ -1,17 +1,39 @@
+import { use } from 'react'
 import './App.css'
 //import Trash from '@../assets/trash.svg'
 
 function App() {
 
+  const users = [
+    {
+      id: '15548415',
+      name: 'Vitor',
+      busisnes: 'Agência Santos',
+      phone: '+351 963096155 '
+    },
+    {
+      id: '1554841587',
+      name: 'França',
+      busisnes: 'VSVale Design',
+      phone: '+55 98840-4216  '
+    },
+    {
+      id: '15548416',
+      name: 'Semila',
+      busisnes: 'Semila Piercer',
+      phone: '+351 963887147'
+    }
+  ]
+
   return (
-    <div className='flex flex-col gap-6'>
-      <div className='bg-gray-200 min-w-md p-12 border-1 border-gray-300 rounded-md'>
+    <div className='flex flex-col gap-2'>
+      <div className='bg-gray-200 min-w-md p-12 border-1 border-gray-100 rounded-md mb-4'>
         <h1 className='font-bold'>CRUD</h1>
         <form className='text-start flex flex-col justify-start'>
-          
+
           <label className=''>Nome:</label>
           <input type="text" name='name' className='bg-white p-3 mb-2' />
-          
+
           <label htmlFor="">Empresa:</label>
           <input type="text" name='email' className='bg-white p-3 mb-2' />
 
@@ -23,17 +45,16 @@ function App() {
         </form>
       </div>
 
-      <div className=''>
-        <div className='min-w-md p-4 border-b-1 border-gray-300 text-start mb-2'>
-          <p><b>Nome:</b> Vitor Santos | Agência Santos <b>Telefone:</b> +351 963 0966 155</p>
+      <b className='text-start'>Clientes</b>
+      {users.map(user => (
+        <div key={user.id}>
+          <div className='min-w-md pb-2 border-1 p-3 border-gray-200 text-start'>
+            <p><b>Nome:</b> {user.name} | {user.busisnes} <b>Telefone:</b> {user.phone}</p>
+          </div>
         </div>
+      ))}
 
-        <div className='min-w-md p-4 border-b-1 border-gray-300 text-start mb-2'>
-          <p><b>Nome:</b> Semila Nozaki | Semila Piercer <b>Telefone:</b> +351 963 0966 155</p>
-        </div>
-      </div>
     </div>
-
   )
 }
 

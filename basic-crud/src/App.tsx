1d9a6d33458a85eb55839e14903ee32c1e6767de
Vitor <vitorsantos.br@gmail.com>
2025-10-13 20:45:55 +0100

    Form Cadastro de Empresa

--- basic-crud/src/App.tsx
+++ basic-crud/src/App.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+//import Trash from '@../assets/trash.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-white font-sans bg-red-500 p-5'>Olá Mundo</h1>
-    </>
+    <div className='flex flex-col gap-6'>
+      <div className='bg-gray-200 min-w-md p-12 border-1 border-gray-300 rounded-md'>
+        <h1 className='font-bold'>CRUD</h1>
+        <form className='text-start flex flex-col justify-start'>
+          
+          <label className=''>Nome:</label>
+          <input type="text" name='name' className='bg-white p-3 mb-2' />
+          
+          <label htmlFor="">Empresa:</label>
+          <input type="text" name='email' className='bg-white p-3 mb-2' />
+
+          <label htmlFor="">Telefone:</label>
+          <input type="text" name='age' className='bg-white p-3 mb-4' />
+
+          <button type='button' className='bg-blue-500 hover:bg-fuchsia-500 font-extrabold'>Cadastrar</button>
+
+        </form>
+      </div>
+
+      <div className=''>
+        <div className='bg-gray-200 min-w-md p-4 border-1 border-gray-300 rounded-md text-start mb-2'>
+          <p>Vitor Santos</p>
+          <p>Agência Santos</p>
+          <p>+351 963 0966 155</p>
+        </div>
+
+        <div className='bg-gray-200 min-w-md p-4 border-1 border-gray-300 rounded-md text-start'>
+          <p>Semila Nozaki</p>
+          <p>Semila Piercer</p>
+          <p>+351 988 157 447</p>
+        </div>
+      </div>
+    </div>
+
   )
 }
 

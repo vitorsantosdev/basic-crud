c1d1fb853130f12a2a50b50cd77f015883ec2740
Vitor <vitorsantos.br@gmail.com>
2025-10-14 20:10:09 +0100

    style: mudar layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ interface User {
 }
 
 function App() {
+
   const [name, setName] = useState('')
   const [business, setBusiness] = useState('')
   const [phone, setPhone] = useState('')
@@ -53,21 +54,21 @@ function App() {
     <div className='flex gap-6 justify-center h-screen bg-gray-50'>
 
       <div className='w-[30%] p-20'>
-        <h2 className='text-2xl mb-8'>Cadastrar Clientes</h2>
+        <h2 className='text-1xl mb-8 uppercase'>Cadastrar Clientes</h2>
 
         <form className='text-stat flex flex-col justify-start' onSubmit={handleSubmit}>
 
           <label className='text-gray-400 text-xs font-bold'>Nome:</label>
-          <input type="text" name='name' className='text-black bg-white p-3 mb-2' value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="text" name='name' className='text-black p-3 border border-gray-300 rounded-md mb-2' value={name} onChange={(e) => setName(e.target.value)} />
 
           <label className='text-gray-400 text-xs font-bold'>Empresa:</label>
-          <input type="text" name='name' className='text-black bg-white p-3 mb-2' value={business} onChange={(e) => setBusiness(e.target.value)} />
+          <input type="text" name='name' className='text-black p-3 border border-gray-300 rounded-md mb-2' value={business} onChange={(e) => setBusiness(e.target.value)} />
 
           <label className='text-gray-400 text-xs font-bold'>Email:</label>
-          <input type="text" name='email' className='text-black bg-white p-3 mb-2' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="text" name='email' className='text-black p-3 border border-gray-300 rounded-md mb-2' value={email} onChange={(e) => setEmail(e.target.value)} />
 
           <label className='text-gray-400 text-xs font-bold'>Telefone:</label>
-          <input type="text" name='age' className='text-black bg-white p-3 mb-4' value={phone} onChange={(e) => setPhone(e.target.value)} />
+          <input type="text" name='age' className='text-black p-3 border border-gray-300 rounded-md mb-2' value={phone} onChange={(e) => setPhone(e.target.value)} />
 
           <button type='submit' className='bg-black hover:bg-gray-500 font-extrabold text-white p-3 rounded-full'>Cadastrar</button>
 
@@ -76,16 +77,16 @@ function App() {
 
 
       <div className='text-start w-[70%] p-20 bg-white'>
-        <h2 className='text-2xl mb-8'>Todos os Clientes (#{updateUser})</h2>
+        <h2 className='text-1xl uppercase mb-8'>Todos os Clientes (#{updateUser})</h2>
 
         <table className='min-w-full'>
           <thead>
             <tr>
-              <th className='pb-3 text-start'>Nome</th>
-              <th className='pb-3 text-start'>Empresa</th>
-              <th className='pb-3 text-start'>Telefone</th>
-              <th className='pb-3 text-start'>Email</th>
-              <th className='pb-3 text-start'>Açoõs</th>
+              <th className='pb-3 text-start text-xs text-gray-400'>Nome</th>
+              <th className='pb-3 text-start text-xs text-gray-400'>Empresa</th>
+              <th className='pb-3 text-start text-xs text-gray-400'>Telefone</th>
+              <th className='pb-3 text-start text-xs text-gray-400'>Email</th>
+              <th className='pb-3 text-start text-xs text-gray-400'>Açoõs</th>
               
             </tr>
           </thead>
@@ -94,11 +95,14 @@ function App() {
 
         {listUsers.map((user, index) => (
             <tr key={index}>
-              <td className=' border-gray-300 pr-5 py-4'>{user.name}</td>
-              <td className=' px-4 border-gray-300 py-4'>{user.business}</td>
-              <td className=' px-4 border-gray-300 py-4'>{user.phone}</td>
-              <td className=' px-4 border-gray-300 py-4'>{user.email}</td>
-              <td className=' px-4 border-gray-300 py-4'>EDITAR / DELETAR</td>
+              <td className=' border-gray-300 pr-5 text-gray-500 py-4'>{user.name}</td>
+              <td className=' px-4 border-gray-300 text-gray-500 py-4'>{user.business}</td>
+              <td className=' px-4 border-gray-300 text-gray-500 py-4'>{user.phone}</td>
+              <td className=' px-4 border-gray-300 text-gray-500 py-4'>{user.email}</td>
+              <td className=' px-4 border-gray-300 text-gray-500 py-4 flex'>
+                <img src="/trash.svg" alt='Deletar' className="w-6 h-6 opacity-30 hover:opacity-100" />
+                <img src="/edit.svg" alt='Editar' className="w-6 h-6 opacity-30 hover:opacity-100" />
+                </td>
             </tr>
         ))}
 
